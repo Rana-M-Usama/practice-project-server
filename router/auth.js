84e6037a5730b5aaea688677ee927a7488b77923
iqra <alishuja519@gmail.com>
2021-08-13 15:55:58 +0500

    Done

--- router/auth.js
+++ router/auth.js
@@ -1,17 +1,43 @@
 const express = require('express')
 const router = express.Router();
 const mysql = require('../db/mysql')
-
-
-router.post('/register', async (req, res) => {   
+var bcrypt = require('bcryptjs');
+require('../db/mysql');
+router.post('/register', async (req, res) => {
     console.log(req.body);
+    const hashpassword = await bcrypt.hash(req.body.password, 10);
+    console.log(hashpassword)
+    const query = `call database1.Register('${req.body.email}', '${hashpassword}', '${req.body.name}','${req.body.phone}');`
+    console.log(query);
+    mysql.query(query, async (err, result) => {
 
+        console.log(err);
+        console.log(result);
+
+        if (err) {
+
+            if (err.errno === 1062) {
+
+
+                res.status(404).send('');
+            }
+
+
+
+        }
+        else {
+
+            res.send('success');
+        }
+
+
+    })
 });
 
 router.post('/signin', async (req, res) => {
 
     console.log(req.body);
-    query = `select password from registration where email='${req.body.email}'`
+    query = `select password from users where Login_id='${req.body.email}'`
     console.log(query);
 
 
@@ -23,14 +49,13 @@ router.post('/signin', async (req, res) => {
         }
         else {
 
+
             var password = req.body.password;
             const comparison = await bcrypt.compare(password, result[0].password)
             if (comparison) {
-
                 res.send('suceess')
             }
             else {
-
                 res.status(401).send('');
             }
 
